a93e915fd6baf15cc45fbc77510db2cac871b452
FrontCoderMike123 <michelBeaubien@hotmail.com>
2016-03-25 04:10:16 -0700

    HUGE changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 (function() {
-var miniSite = angular.module('miniSite', ['ui.router','ngSanitize','ngAnimate']);
+var miniSite = angular.module('miniSite', ['ui.router','ngSanitize']);
 
 miniSite.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
 	$stateProvider.state('About',{
@@ -17,6 +17,11 @@ miniSite.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$
 		templateUrl: 'views/features.html',
 		controller: 'featuresCTRL'
 	});
+	$stateProvider.state('Tour',{
+		url: '/tour',
+		templateUrl: 'views/tour.html',
+		controller: 'tourCTRL'
+	});
 	$stateProvider.state('Developer',{
 		url: '/developer',
 		templateUrl: 'views/developer.html',
@@ -31,34 +36,41 @@ miniSite.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$
 //either the sections and divs would float above eachother .... or the nav would turn on and off....
 //too see for yourself... just add in the UL in index.html. ng-controller="NavCtrl"... see for yourslef
 
-miniSite.controller('NavCtrl',['$scope','$http','$location',function($scope,$http,$location){
-	$scope.getClass = function(path) {
-    var cur_path = $location.path().substr(0, path.length);
-    if (cur_path == path) {
-        if($location.path().substr(0).length > 1 && path.length == 1 )
-            return "";
-        else
-            return "active";
-    } else {
-        return "";
-    }
-}
+miniSite.controller('navCTRL',['$scope','$http','$location',function($scope,$http,$location){
+
+}]);
+
+miniSite.controller('homeCTRL',['$scope','$http','$location',function($scope,$http,$location){
+
 }]);
 
 miniSite.controller('aboutCTRL',['$scope','$http',function($scope,$http){
-	
+
 }]);
 
 miniSite.controller('techCTRL',['$scope','$http',function($scope,$http){
 	$http.get('tech.json').success(function(data) {
-		$scope.techs = data.Tech;
+		$scope.techs = data.Techs;
 	});
+	var button = document.querySelector('#techStagger');
+	function toolList() {
+		var holder = document.querySelectorAll('.listHolder');
+		for(var i=0; i<holder.length; i++){
+			holder[i].classList.add('block');
+		}
+	TweenMax.staggerFrom(".tween",2,{scale:0.5,opacity:0,delay:0.5,ease:Elastic.easeOut}, 0.1);
+	};
+    button.addEventListener('click',toolList,false);
 }]);
 
 miniSite.controller('featuresCTRL',['$scope','$http',function($scope,$http){
 	$http.get('features.json').success(function(data) {
 		$scope.features = data.Features;
 	});
+}]);
+
+miniSite.controller('tourCTRL',['$scope','$http',function($scope,$http){
+
 }]);
 
 miniSite.controller('developerCTRL',['$scope','$http',function($scope,$http){
